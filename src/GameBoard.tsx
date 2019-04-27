2d8e874cf04f7565c243a37658844e923a19152d
Noah Allen <noahtallen@gmail.com>
2019-04-27 14:43:51 -0400

    Allow user to tap on a cell to change it

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -3,18 +3,22 @@ import styled from 'styled-components';
 import { OthelloBoard, OthelloCell } from './models';
 
 type BoardProps = {
-    gameState: OthelloBoard
+    gameState: OthelloBoard,
+    onClickCell: (i: number, j: number) => void
 }
 
 // Note: I used the indices for keys in this case because the index
 // of an individual cell or row should never change. Though you'd
 // nromally have a real id of some sort, the unique identifier in
 // this case is actually the index!. 
-export default function GameBoard({ gameState }: BoardProps) {
+export default function GameBoard({ gameState, onClickCell }: BoardProps) {
     return <Table>
         <tbody>{gameState.map((row, i) =>
             <tr key={i} >
-                {row.map((cell, j) => <GameCell cell={cell} key={j} />)}
+                {row.map((cell, j) => {
+                    const boundCellClickHandler = () => onClickCell(i, j)
+                    return <GameCell cell={cell} key={j} onClick={boundCellClickHandler}/>
+                })}
             </tr>
         )}</tbody>
     </Table>
@@ -22,13 +26,14 @@ export default function GameBoard({ gameState }: BoardProps) {
 
 type CellProps = {
     cell: OthelloCell,
+    onClick: () => void
 }
-const GameCell = ({ cell }: CellProps) => {
+const GameCell = ({ cell, onClick }: CellProps) => {
     let content = null
     if (cell !== OthelloCell.Empty) {
         content = <Puck isWhite={cell === OthelloCell.White} />
     }
-    return <Cell>{content}</Cell>
+    return <Cell onClick={onClick}>{content}</Cell>
 }
 
 const Table = styled.table`
@@ -49,7 +54,7 @@ type PuckProps = {
     isWhite: boolean
 }
 const puckSize = cellSize - 20
-const Puck = styled.div<PuckProps>`
+export const Puck = styled.div<PuckProps>`
     margin: auto;
     box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
     width: ${puckSize}px;
--- src/GameContainer.tsx
+++ src/GameContainer.tsx
@@ -1,44 +1,70 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import GameBoard from './GameBoard';
+import GameInfo from './GameInfo'
 import { OthelloCell, OthelloBoard } from './models';
 
 type Props = {
-    boardSize: number
+    boardSize: number,
+    playerColor: OthelloCell
 }
 
-function generateGameBoard(boardSize: number): OthelloBoard {
+function setInitialCells(board: OthelloBoard, playerColor: OthelloCell): OthelloBoard {
+    // Set the initial game pieces:
+    const center1 = board.length / 2 - 1
+    const center2 = board.length / 2
+    const aiColor = playerColor === OthelloCell.White ? OthelloCell.Black : OthelloCell.White
+    board[center1][center1] = playerColor
+    board[center1][center2] = aiColor
+    board[center2][center1] = aiColor
+    board[center2][center2] = playerColor
+    return board
+}
+
+function generateGameBoard(boardSize: number, playerColor: OthelloCell): OthelloBoard {
     const getEmpties = () => Array(boardSize).fill(OthelloCell.Empty)
     // We do a fill and then a map in order to create new instances
     // of the empties array. Otherwise, each row will reference the
     // same object in memory.
     const fullBoard = Array(boardSize).fill(0).map( getEmpties )
-
-    // Set the initial game pieces:
-    const center1 = boardSize / 2 - 1
-    const center2 = boardSize / 2
-    fullBoard[center1][center1] = OthelloCell.White
-    fullBoard[center1][center2] = OthelloCell.Black
-    fullBoard[center2][center1] = OthelloCell.Black
-    fullBoard[center2][center2] = OthelloCell.White
-
-    console.log(fullBoard)
-    return fullBoard
+    return setInitialCells(fullBoard, playerColor)
 }
 
-export default function GameContainer({ boardSize }: Props) {
+// Call this function to update the board with new info.
+// board is the existing board.
+// i is the row index
+// j is the column index
+// user is the user (i.e. OthelloCell.Black) who "clicked" the cell
+function handleCellClick(board: OthelloBoard, i: number, j: number, user: OthelloCell): OthelloBoard {
+    const newBoard = [ ...board ] // Needs to be a new memory reference or it won't update
+    newBoard[i][j] = user
+    // @TODO: handle the conversion of existing cells
+    return newBoard
+}
+
+export default function GameContainer({ boardSize, playerColor }: Props) {
     if (boardSize % 2 !== 0) {
         throw new Error('Board size was not an even number.')
     }
     const [isPlaying, setIsPlaying] = React.useState(false)
     const [gameState, setGameState] = React.useState<OthelloBoard | null>(null)
     const startGame = () => {
-        setGameState(generateGameBoard(boardSize))
+        setGameState(generateGameBoard(boardSize, playerColor))
         setIsPlaying(true)
     }
+
+    const onClickCell = (i: number, j: number) => {
+        if (isPlaying && gameState) {
+            const newBoard = handleCellClick(gameState, i, j, playerColor)
+            console.log(newBoard)
+            setGameState(newBoard)    
+        }
+    }
+
     return <Wrapper>
         {!isPlaying && <StartButton onClick={startGame}>Start a Game</StartButton>}
-        {isPlaying && gameState && <GameBoard gameState={gameState} />}
+        {isPlaying && gameState && <GameInfo playerColor={playerColor} />}
+        {isPlaying && gameState && <GameBoard gameState={gameState} onClickCell={onClickCell}/>}
     </Wrapper>
 }
 
