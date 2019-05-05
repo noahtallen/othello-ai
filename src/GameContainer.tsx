3a4d5daa433307ade8f157b97de232f9715ea763
Noah Allen <noahtallen@gmail.com>
2019-05-05 16:49:31 -0400

    Play against a human

--- src/GameContainer.tsx
+++ src/GameContainer.tsx
@@ -15,35 +15,40 @@ export default function GameContainer({ boardSize, playerColor, aiKind }: Props)
     if (boardSize % 2 !== 0 || boardSize < 0) {
         throw new Error('Board size is not a valid number.')
     }
-    const aIColor = Reversi.getOppositeCellType(playerColor)
+    const player2Color = Reversi.getOppositeCellType(playerColor)
     const [gameState, setGameState] = React.useState<ReversiBoard>(Reversi.generateGameBoard(boardSize, playerColor))
     const [score, setScore] = React.useState<Scores>(Reversi.countScores(gameState))
-    const [isAiPlaying, setIsAiPlaying] = React.useState(false)
+    const [isPlayer2Turn, setisPlayer2Turn] = React.useState(false)
     const [infoMessage, setInfoMessage] = React.useState<string>('')
 
     const startAiMove = async () => {
-        if (!isAiPlaying) {
-            setIsAiPlaying(true)
+        if (!isPlayer2Turn) {
+            setisPlayer2Turn(true)
             try {
                 setInfoMessage('')
-                const newBoard = await Reversi.makeAiMove(gameState, aiKind, aIColor)
+                const newBoard = await Reversi.makeAiMove(gameState, aiKind, player2Color)
                 setGameState(newBoard)
                 setScore(Reversi.countScores(newBoard))
             } catch (e) {
                 setInfoMessage('The AI couldn\'t make a move')
             }
-            setIsAiPlaying(false)
+            setisPlayer2Turn(false)
         }
     }
 
     const onClickCell = (coord: Coordinate) => {
         setInfoMessage('')
-        if (!isAiPlaying) {
-            const newBoard = Reversi.handleCellClick(gameState, coord, playerColor)
+        if (!isPlayer2Turn || aiKind === AIKind.Human) {
+            const player = isPlayer2Turn ? player2Color : playerColor
+            const newBoard = Reversi.handleCellClick(gameState, coord, player)
             if (newBoard) {
                 setGameState(newBoard)    
                 setScore(Reversi.countScores(newBoard))
-                startAiMove()    
+                if (aiKind !== AIKind.Human) {
+                    startAiMove()    
+                } else {
+                    setisPlayer2Turn(!isPlayer2Turn)
+                }
             } else {
                 setInfoMessage('Invalid move. Please try again.')
             }
@@ -56,11 +61,11 @@ export default function GameContainer({ boardSize, playerColor, aiKind }: Props)
             playerColor={playerColor}
             score={score}
             aiKind={aiKind}
-            currentTurn={isAiPlaying ? aIColor : playerColor} />
+            currentTurn={isPlayer2Turn ? player2Color : playerColor} />
         <GameBoard
             gameState={gameState}
             onClickCell={onClickCell}
-            isAiPlaying={isAiPlaying}
+            isAiPlaying={isPlayer2Turn}
             infoMessage={infoMessage}
             score={score}
             playerColor={playerColor}/>
